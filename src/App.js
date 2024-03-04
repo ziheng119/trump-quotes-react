f86d65bf3ab3b40347dfcc3cf6c9b48ce30b4276
Zi Heng <ziheng@Zis-MacBook-Pro.local>
2024-03-05 00:15:50 +0800

    Changed to Trump Quotes

--- src/App.js
+++ src/App.js
@@ -7,16 +7,16 @@ class App extends React.Component {
   state = { advice: " " };
 
   componentDidMount() {
-    this.fetchAdvice();
+    this.fetchQuote();
   }
 
-  fetchAdvice = () => {
+  fetchQuote = () => {
     // No need const as it is a function inside a class, which makes it a method that belongs to the class
     axios
-      .get("https://api.adviceslip.com/advice")
+      .get("https://api.tronalddump.io/random/quote")
       .then((response) => {
-        const { advice } = response.data.slip;
-        this.setState({ advice: advice });
+        const { value } = response.data; // This line extracts the value property from the response data object. The response data is assumed to be in JSON format, and it seems to contain the value property.
+        this.setState({ advice: value }); // Sets the value of the 'advice' property in the component's state to the value of the 'value' property received from the API response
       })
       .catch((error) => {
         console.log(error);
@@ -29,8 +29,8 @@ class App extends React.Component {
       <div className="app">
         <div className="card">
           <h1 className="heading">{advice}</h1>
-          <button className="button" onClick={this.fetchAdvice}>
-            <span>GIVE ME ADVICE!</span>
+          <button className="button" onClick={this.fetchQuote}>
+            <span>MAKE AMERICA GREAT AGAIN!</span>
           </button>
         </div>
       </div>
